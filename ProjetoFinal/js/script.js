a83d811dcafae54f7cca38ec4770d3334e704192
GleidsonLM <gleidsonlm@gmail.com>
2023-08-21 18:55:53 -0300

    fix: moved reset game and color mode to external files functions

--- ProjetoFinal/js/script.js
+++ ProjetoFinal/js/script.js
@@ -1,4 +1,6 @@
 import { createGame } from './createGame.function.js';
+import { resetGame } from './resetGame.function.js';
+import { setColorMode } from './setColorMode.function.js';
 
 let isGameRunning = false;
 window.addEventListener('load', () => {
@@ -11,25 +13,6 @@ window.addEventListener('load', () => {
     }
 });
 
-let resetGameButton = document.getElementById('reset');
-resetGameButton.addEventListener('click', () => {
-    window.location.reload();
-});
+resetGame();
 
-/* Toogle Color Mode */
-let colorModeButton = document.getElementById('bd-theme');
-colorModeButton.addEventListener('click', () => {
-    let theme = document.documentElement.getAttribute('data-bs-theme');
-    let newTheme = theme === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-bs-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
-    
-    let forcaStroke = document.getElementById('forca');
-    if (theme === 'dark') { 
-        forcaStroke.classList.add('stroke-night')
-        forcaStroke.classList.remove('stroke-day')
-    } else {
-        forcaStroke.classList.remove('stroke-night')
-        forcaStroke.classList.add('stroke-day')
-    }
-});
+setColorMode();
